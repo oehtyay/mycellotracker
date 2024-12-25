d793318979b530a0e9edf822ce94e0285d261780
oehtyay <145249937+oehtyay@users.noreply.github.com>
2024-12-25 15:31:07 +1000

    Add files via upload

--- intro.js
+++ intro.js
@@ -2,19 +2,13 @@ function findName(event) {
     event.preventDefault(); 
 
     const username = document.getElementById('input').value;
-    if (username != '' && username.toUpperCase() != 'THEO' && username.toUpperCase() != 'EVA') {
+    if (username != '' && username.toUpperCase() != 'THEO') {
         localStorage.setItem('username', username);
         setTimeout(() => {
             window.location.href = 'index.html'; 
         }, 1000);
     } else if (username.toUpperCase() == 'THEO') {
         alert("Broski, I know your name is not Theo.")
-    } else if (username.toUpperCase() == 'EVA') {
-        alert("Aww thanks for checking it out again. Love you :)")
-        localStorage.setItem('username', username);
-        setTimeout(() => {
-            window.location.href = 'index.html'; 
-        }, 1000);
     } else {
         alert("Please enter a name.")
     }
